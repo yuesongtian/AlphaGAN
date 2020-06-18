baabdf1a6878ad7f004cadc0468833a0c79b1ab3
yuesongtian <yuesongtian@163.com>
2020-06-18 21:15:22 +0800

    Create __init__.py

---
+++ models/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import models.building_blocks
+import models.alphaGAN_network
+import models.genotypes
+import models.operations_gan
+import models.model_search_gan
