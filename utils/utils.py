e34805041ddcc8f655e32995fbd98c9dd9072f21
yuesongtian <yuesongtian@163.com>
2020-06-18 21:12:39 +0800

    Add files via upload

---
+++ utils/utils.py
@@ -0,0 +1,166 @@
+import os
+import numpy as np
+import torch
+import shutil
+import time
+import logging
+import torchvision.transforms as transforms
+import dateutil.tz
+from torch.autograd import Variable
+from datetime import datetime
+
+
+def _data_transforms_cifar10(args):
+  CIFAR_MEAN = [0.49139968, 0.48215827, 0.44653124]
+  CIFAR_STD = [0.24703233, 0.24348505, 0.26158768]
+
+  train_transform = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize(CIFAR_MEAN, CIFAR_STD),
+  ])
+  if args.cutout:
+    train_transform.transforms.append(Cutout(args.cutout_length))
+
+  valid_transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(CIFAR_MEAN, CIFAR_STD),
+    ])
+  return train_transform, valid_transform
+
+def _data_transforms_stl10_resize(args, img_size):
+
+  train_transform = transforms.Compose([
+                transforms.Resize(args.img_size),
+                transforms.ToTensor(),
+                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+            ])
+  if args.cutout:
+    train_transform.transforms.append(Cutout(args.cutout_length))
+
+  valid_transform = transforms.Compose([
+                transforms.Resize(args.img_size),
+                transforms.ToTensor(),
+                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+            ])
+  return train_transform, valid_transform
+
+def _data_transforms_cifar10_resize(args, img_size):
+  CIFAR_MEAN = [0.49139968, 0.48215827, 0.44653124]
+  CIFAR_STD = [0.24703233, 0.24348505, 0.26158768]
+
+  train_transform = transforms.Compose([
+    transforms.Resize(img_size),
+    transforms.ToTensor(),
+    transforms.Normalize(CIFAR_MEAN, CIFAR_STD),
+  ])
+  if args.cutout:
+    train_transform.transforms.append(Cutout(args.cutout_length))
+
+  valid_transform = transforms.Compose([
+    transforms.Resize(img_size),
+    transforms.ToTensor(),
+    transforms.Normalize(CIFAR_MEAN, CIFAR_STD),
+    ])
+  return train_transform, valid_transform
+
+def count_parameters_in_MB(model):
+  return np.sum(np.prod(v.size()) for name, v in model.named_parameters() if "auxiliary" not in name)/1e6
+
+
+def save_checkpoint(state, is_best, save):
+  filename = os.path.join(save, 'checkpoint.pth.tar')
+  torch.save(state, filename)
+  if is_best:
+    best_filename = os.path.join(save, 'model_best.pth.tar')
+    shutil.copyfile(filename, best_filename)
+
+
+def save(model, model_path):
+  torch.save(model.state_dict(), model_path)
+
+
+def load(model, model_path):
+  model.load_state_dict(torch.load(model_path))
+
+def create_exp_dir(path, scripts_to_save=None):
+  if not os.path.exists(path):
+    os.mkdir(path)
+  print('Experiment dir : {}'.format(path))
+
+  if scripts_to_save is not None:
+    os.mkdir(os.path.join(path, 'scripts'))
+    for script in scripts_to_save:
+      dst_file = os.path.join(path, 'scripts', os.path.basename(script))
+      shutil.copyfile(script, dst_file)
+
+def create_sub_dir(path):
+  path_helper = {}
+
+  logdir = os.path.join(path, 'log')
+  if not os.path.exists(logdir):
+    os.mkdir(logdir)
+  path_helper['log'] = logdir
+
+  sample_dir = os.path.join(path, 'sample')
+  if not os.path.exists(sample_dir):
+    os.mkdir(sample_dir)
+  path_helper['sample_path'] = sample_dir
+
+  model_dir = os.path.join(path, 'model')
+  if not os.path.exists(model_dir):
+    os.mkdir(model_dir)
+  path_helper['model'] = model_dir
+
+  return path_helper
+
+def create_logger(log_dir, phase='train'):
+    time_str = time.strftime('%Y-%m-%d-%H-%M')
+    log_file = '{}_{}.log'.format(time_str, phase)
+    final_log_file = os.path.join(log_dir, log_file)
+    head = '%(asctime)-15s %(message)s'
+    logging.basicConfig(filename=str(final_log_file),
+                        format=head)
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    console = logging.StreamHandler()
+    logging.getLogger('').addHandler(console)
+
+    return logger
+
+
+def set_log_dir(root_dir, exp_name):
+    path_dict = {}
+    os.makedirs(root_dir, exist_ok=True)
+
+    # set log path
+    exp_path = os.path.join(root_dir, exp_name)
+    now = datetime.now(dateutil.tz.tzlocal())
+    timestamp = now.strftime('%Y_%m_%d_%H_%M_%S')
+    prefix = exp_path + '_' + timestamp
+    os.makedirs(prefix)
+    path_dict['prefix'] = prefix
+
+    # set checkpoint path
+    ckpt_path = os.path.join(prefix, 'Model')
+    os.makedirs(ckpt_path)
+    path_dict['ckpt_path'] = ckpt_path
+
+    log_path = os.path.join(prefix, 'Log')
+    os.makedirs(log_path)
+    path_dict['log_path'] = log_path
+
+    # set sample image path for fid calculation
+    sample_path = os.path.join(prefix, 'Samples')
+    os.makedirs(sample_path)
+    path_dict['sample_path'] = sample_path
+
+    return path_dict
+
+
+def save_checkpoint(states, is_best, output_dir,
+                    filename='checkpoint.pth'):
+    torch.save(states, os.path.join(output_dir, filename))
+    if is_best:
+        torch.save(states, os.path.join(output_dir, 'checkpoint_best.pth'))
